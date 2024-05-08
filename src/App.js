546c0f8f445d3a1251a8cb167d4c6ceb51d9842d
JoonYoungKang <abfhs96@codef.io>
2024-05-08 17:03:33 +0900

    20240508

--- src/App.js
+++ src/App.js
@@ -1,94 +1,11 @@
-import React from 'react';
 import '@picocss/pico';
-import SearchBar from './components/SearchBar';
-import PDFViewer from './components/PDFViewer';
+import React, { useState } from 'react';
 import './styles/App.css'; // CSS 파일 임포트
-
-function Portpolio() {
-  // 주소 상태를 관리하는 useState
-  const [address, setAddress] = useState('');
-  const [addrObj, setAddrObj] = useState(null);
-  const [addrkey, setAddrkey] = useState("");
-
-  const [dongObj, setDongObj] = useState(null);
-  const [dongkey, setDongkey] = useState("");
-
-  const [hoObj, setHoObj] = useState(null);
-  const [hokey, setHokey] = useState(null);
-
-  const [pdfHex, setpdfHex] = useState("");
-  
-  
-  let printConsole = () => {
-    var pdfHex = localStorage.getItem('pdfHex');
-    setpdfHex(pdfHex);
-    <PdfViewer hexString={pdfHex} />
-    console.log(JSON.stringify(addrObj));
-    console.log(JSON.stringify(addrkey));
-    console.log(JSON.stringify(hokey));
-  }
-
-
-  // 주소 입력 시 세움터 로그인하고 주소 검색해주는 함수
-  let handleAddressSearch = () => {
-    console.log(address);
-
-    sendPostRequest(address).then(() => {
-      const addrData = localStorage.getItem('addrObj');
-      if (addrData) {
-        setAddrObj(JSON.parse(addrData));
-      }
-    });
-  };
-
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div>건축물대장발급</div>
-      </div>
-
-      <div style={{ marginTop: "20px" }}></div>
-
-      {/* 주소 업데이트 함수를 주소입력창에 전달 */}
-      <주소입력창 setAddress={setAddress} handleAddressSearch={handleAddressSearch} />
-      {addrObj && <주소선택창 addrObj={addrObj} addrkey={addrkey} setAddrkey={setAddrkey} setDongObj={setDongObj} />}
-      {dongObj && <동선택창 dongObj={dongObj} dongkey={dongkey} setDongkey={setDongkey} setHoObj={setHoObj}/>}
-      {hoObj && <호선택창 hoObj={hoObj} hokey={hokey} setHokey={setHokey} setpdfHex={setpdfHex} />}
-
-      <button onClick={() => printConsole()}>주소 잘 입력됬나 확인</button>
-      {pdfHex && <PdfViewer hexString={pdfHex} />}
-
-
-    </div>
-  );
-}
-
-
 
 function App() {
 
-  
   const [address, setAddress] = useState('');
   const [addrObj, setAddrObj] = useState(null);
-  const [addrkey, setAddrkey] = useState("");
-
-  const [dongObj, setDongObj] = useState(null);
-  const [dongkey, setDongkey] = useState("");
-
-  const [hoObj, setHoObj] = useState(null);
-  const [hokey, setHokey] = useState(null);
-
-  const [pdfHex, setpdfHex] = useState("");
-  
-  
-  let printConsole = () => {
-    var pdfHex = localStorage.getItem('pdfHex');
-    setpdfHex(pdfHex);
-    <PdfViewer hexString={pdfHex} />
-    console.log(JSON.stringify(addrObj));
-    console.log(JSON.stringify(addrkey));
-    console.log(JSON.stringify(hokey));
-  }
 
 
   // 주소 입력 시 세움터 로그인하고 주소 검색해주는 함수
@@ -102,8 +19,6 @@ function App() {
       }
     });
   };
-
-
 
 
   return (
@@ -126,16 +41,42 @@ function App() {
               <h3>원하는 위치와 조건을 검색해 보세요.</h3>
             </hgroup>
             <p>아래 검색 창에 원하는 조건을 입력하고 검색 버튼을 클릭하세요.</p>
-            <SearchBar />
+            <주소입력창 setAddress={setAddress} handleAddressSearch={handleAddressSearch} />
+            <form action="#" method="get">
+              <input type="text" id="search" name="search" placeholder="검색어 입력..." aria-label="검색어" required />
+              <button type="submit">검색</button>
+            </form>
             <h3>문서 보기</h3>
             <p>관련 문서를 PDF 형태로 확인할 수 있습니다.</p>
-            <PDFViewer src="sample.pdf" />
+            {/* <PDFViewer src="sample.pdf" /> */}
           </section>
         </main>
       </div>
       <footer className="container">
         <small><a href="#">개인정보처리방침</a> • <a href="#">이용약관</a></small>
       </footer>
+    </div>
+  );
+}
+
+function 주소입력창({ setAddress, handleAddressSearch }) {
+  return (
+    <div>
+
+      <form action="#" method="get">
+        <input type="text" id="search" name="search" placeholder="검색어 입력..." aria-label="검색어" required />
+        <button type="submit">검색</button>
+      </form>
+      <Form.Label htmlFor="inputAddress">주소를 입력하세요</Form.Label>
+      <Form.Control
+        type="text"
+        id="inputAddress"
+        aria-describedby="addressHelpBlock"
+        placeholder="주소를 입력하고 '주소검색' 버튼을 누르세요."
+        style={{ width: '60%', margin: '0 auto' }}
+        onChange={(e) => setAddress(e.target.value)} // 입력 값에 따라 주소 상태 업데이트
+      />
+      <Button variant="outline-dark" onClick={() => handleAddressSearch()}>주소검색</Button>
     </div>
   );
 }
